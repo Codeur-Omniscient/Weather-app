44efccf1385fac82cea77030141bf200a1b1c20d
Codeur-Omniscient <Anthonymedin258@outlook.fr>
2024-03-30 00:14:21 +0100

    [ADD]:Ajout de la notification et implementation de la fct pour recuperer la meteo

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
 "use strick";
+
+// Variable Globales des données
+const apiKey = "443f01183af97e8fb89dfdca06343a5d";
+const city = "london"; // document.querySelector(".search-input").value;
+
+// Implementation des fonction
+
+const displayWeather = function (data) {};
+
+const displayForecast = function (data) {};
+
+const getWeather = function () {
+  //   if (!city) {
+  //     alert("Entrez correctement le nom de la ville");
+  //     return;
+  //   }
+  const currentDataUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+  const forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`;
+
+  fetch(currentDataUrl)
+    .then((response) => response.json())
+    .then((data) => console.log(data))
+    .catch((err) => {
+      console.error(err);
+    });
+
+  fetch(forecastUrl)
+    .then((response) => response.json())
+    .then((data) => data)
+    .catch((err) => {
+      console.error(err);
+    });
+};
+
+getWeather();
